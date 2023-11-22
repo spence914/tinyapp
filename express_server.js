198e179b54349c338ae7ed155466d01e92b041eb
Spencer Lewis <spence.914@gmail.com>
2023-11-22 13:08:15 -0800

    pass username to all views that use header using templateVars

--- express_server.js
+++ express_server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
 
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 
-const generateRandomString = function() {
+const generateRandomString = function () {
   let result = "";
   const characters = "ABCDEFHIJKLMNOPQRSTUVWXYSabcdefghijklmnopqrstuvwxyz1234567890";
   for (let i = 0; i < 6; i++) {
@@ -33,16 +35,23 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new"), templateVars;
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: `${urlDatabase[req.params.id]}` };
+  const templateVars = {
+    id: req.params.id, longURL: `${urlDatabase[req.params.id]}`,
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
 
