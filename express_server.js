29bec9ef513f658ebf535095f473858472744992
Spencer Lewis <spence.914@gmail.com>
2023-11-24 12:40:34 -0800

    add endpoint for /register that renders the urls_register template

--- express_server.js
+++ express_server.js
@@ -93,6 +93,12 @@ app.post("/logout/", (req, res) => {
   res.redirect("/urls/");
 });
 
+app.get("/register", (req, res) => {
+  const templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_register", templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
